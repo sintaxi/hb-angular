7370b3a9cf6607c585f526e149870e935922f0a7
Brock Whitten <brock@chloi.io>
2014-04-06 03:44:28 -0700

    good angular start point

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
-// you put your code in here
+
+angular.module("ToDoApp", [])
+
+  .factory("Items", function(){
+    return [{ msg: "heyo" }]
+  })
+
+  .controller("crud", function($scope, Items){
+    $scope.items = Items
+
+    $scope.add = function(){
+      Items.push({ msg: $scope.item.msg })
+    }
+
+    $scope.remove = function(){
+      Items.pop()
+    }
+  })
